edebc37e63029f2dae32f350fb609acf0a4366f3
cgnitash <cgnitash@gmail.com>
2018-10-18 14:24:30 -0400

    added replicate environment

---
+++ user/environment/replicate.cpp
@@ -0,0 +1,39 @@
+
+#include "replicate.h"
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <numeric>
+#include <random>
+#include <regex>
+#include <string>
+#include <string_view>
+#include <utility>
+#include <vector>
+
+life::eval_results replicate::evaluate(const std::vector<life::entity> &pop) {
+
+ // std::cout << "here1 " << std::flush;
+  auto env = life::make_environment(env_name_);
+  env.configure(env_config_);
+  std::vector<double> temp_scores(pop.size(), 0);
+  for (auto i = 0; i < num_; i++) {
+    auto s = env.evaluate(pop);
+    std::transform(std::begin(s), std::end(s), std::begin(temp_scores),
+                   std::begin(temp_scores),
+                   [](const auto &org_s, const auto c_s) {
+                     return c_s + std::stod(org_s.second.at("score"));
+                   });
+  }
+
+  life::eval_results scores;
+  std::transform(std::begin(pop), std::end(pop), std::begin(temp_scores),
+                 std::back_inserter(scores), [this](const auto &org, const auto score) {
+                   std::map<std::string, std::string> m;
+                   m["score"] = std::to_string(score / num_);
+                   return std::make_pair(org, m);
+                 });
+ // std::cout << "here 3" << std::flush;
+  return scores;
+}
+
---
+++ user/environment/replicate.h
@@ -0,0 +1,40 @@
+
+#include"../../components.h"
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <numeric>
+#include <string>
+#include <string_view>
+#include <vector>
+#include <random>
+#include <regex>
+#include <set>
+#include <initializer_list>
+
+class replicate {
+
+	long num_ = 1;
+	std::string env_name_ = "null_environment";
+	life::configuration env_config_;
+public:
+  replicate() {
+	  configure(publish_configuration());
+  }
+
+  life::configuration publish_configuration() {
+    life::configuration c;
+	c["num"] = num_;
+	c["env"] = {env_name_,{}};
+    return c;
+  }
+
+  void configure(life::configuration c) {
+    num_ = c["num"];
+    env_name_ = c["env"][0];
+	env_config_ = c["env"][1];
+  }
+
+  life::eval_results evaluate(const std::vector<life::entity> &);
+};
+
