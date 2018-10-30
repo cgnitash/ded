c4d39ef47377799de2857e1cb7a99a75d9286ff6
cgnitash <cgnitash@gmail.com>
2018-10-30 14:31:48 -0400

    added repeat loop utility

--- core/utilities.h
+++ core/utilities.h
@@ -7,4 +7,10 @@ namespace util {
 inline long Bit(double d) { return d > 0 ? 1 : 0; }
 
 const auto PI = std::atan(1) * 4;
+
+template <typename F> void repeat(size_t n, F f) {
+  while (n--)
+    f();
+}
+
 } // namespace util
