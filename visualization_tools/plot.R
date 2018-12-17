f7a25847022cd150d1ea19344d2c79dfdf0a79ea
cgnitash <cgnitash@gmail.com>
2018-12-17 13:50:23 -0500

    added some simple vis tools

--- visualization_tools/plot.R
+++ visualization_tools/plot.R
@@ -4,16 +4,16 @@ av_data = read.csv(paste("data_",2,".csv",sep=""))
 p = ggplot(data=av_data) + geom_line(aes(x=update,y=avg)) + geom_line(aes(x=update,y=max)) + xlab("update") + ylab("score")
 p
 
-across_reps = function(prefix,range) {
-  files = lapply(range, function(i) paste(prefix,i,".csv",sep=""))
+across_reps = function(range) {
+  files = lapply(range, function(i) paste("data/data_",i,"/pop.csv",sep=""))
   ldply(lapply(files,read.csv),rbind)
 }
 
-report = function(data, var) {
+report = function(var,data) {
   var = enquo(var)
   data %>% 
   group_by(update) %>%
-  summarise(
+  dplyr::summarise(
       N = n(),
       mean = mean(!! var),
       median = median(!! var),
@@ -22,9 +22,7 @@ report = function(data, var) {
   )
 }
 
-agg_data = report(across_reps("data_",101:110),avg)
+agg_data = report(avg,across_reps(10:13))
 
-p = ggplot(data=agg_data,aes(x=update,y=mean)) + geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=.01)  + geom_line() + xlab("update") + ylab("fitness") 
+p = ggplot(data=agg_data,aes(x=update,y=mean)) + geom_errorbar(aes(ymin=mean-se, ymax=mean+se), color="gray",width=.01)  + geom_line() + xlab("update") + ylab("fitness") 
 p
-
-
