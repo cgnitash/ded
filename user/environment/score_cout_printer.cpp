06968a81f961ec6a3d4dab6d4ff01e8ecd0dbfc1
cgnitash <cgnitash@gmail.com>
2019-01-07 19:34:01 -0500

    oops, fixed

--- user/environment/score_cout_printer.cpp
+++ user/environment/score_cout_printer.cpp
@@ -15,8 +15,7 @@
 life::population score_cout_printer::evaluate(life::population pop) {
 
   // make and configure nested environment
-  auto env = life::make_environment(env_name_);
-  env.configure(env_config_);
+  auto env = life::make_environment(env_);
 
   for (const auto &org : env.evaluate(pop).get_as_vector())
     std::cout << (message_ ? "\nhi " : "\n ") << org.get_id() << " "
