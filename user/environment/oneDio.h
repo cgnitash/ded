80309d1f94a5221a22cf2b74355e72553d8a0ad8
cgnitash <cgnitash@gmail.com>
2019-02-11 17:31:51 -0500

    oned world to test cppn

---
+++ user/environment/oneDio.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "../../components.h"
+
+#include <algorithm>
+#include <fstream>
+#include <initializer_list>
+#include <iostream>
+#include <numeric>
+#include <random>
+#include <regex>
+#include <set>
+#include <string>
+#include <string_view>
+#include <vector>
+
+class oneDio {
+
+  std::string org_input_tag_  = "in,A<double,1>";
+  std::string org_output_tag_ = "out,A<double,1>";
+
+public:
+  oneDio() { configure(publish_configuration()); }
+
+  life::configuration publish_configuration()
+  {
+    life::configuration c;
+
+    c["parameters"] = nullptr;
+
+    c["pre-tags"] = nullptr;
+
+    c["post-tags"] = nullptr;
+
+    c["input-tags"]["in"]   = org_input_tag_;
+
+    c["output-tags"]["out"] = org_output_tag_;
+
+    return c;
+  }
+
+  void configure(life::configuration con) {
+    org_input_tag_  = con["input-tags"]["in"];
+    org_output_tag_ = con["output-tags"]["out"];
+  }
+
+  life::population evaluate(life::population);
+};
