144de9f595c9e43a7ceabb5edc5035c037e89d9b
cgnitash <cgnitash@gmail.com>
2019-02-20 18:28:31 -0500

    more plotting

--- visualization_tools/plot.R
+++ visualization_tools/plot.R
@@ -18,7 +18,7 @@ aggregate_population = function(path,cycle_range,recorded_tag) {
 
 
 across_reps = function(file,component_name,recorded_tag,rep_range,cycle_range) {
-  paths = lapply(rep_range, function(i) paste(file,"REP_",i,"/",component_name,"/",sep=""))
+  paths = lapply(rep_range, function(i) paste(file,"REP_",i,"/pass_through/",component_name,"/",sep=""))
   plyr::ldply(lapply(paths,aggregate_population,cycle_range=cycle_range,recorded_tag=recorded_tag),rbind)
 }
 
@@ -81,6 +81,43 @@ cluster_plots = function(all_exps,ylabel,labs,indices,cols) {
   master_plot
 }
 
+extract_row = function(row,col) {
+  mean = paste("mean",col,sep="")
+  se = paste("se",col,sep="")
+  c(col,row[[mean]],row[[mean]]+row[[se]],row[[mean]]-row[[se]])
+}
+
+final_fitness_plots = function(all_exps,ylabel,x_labs) {
+  all_exps = all_avg
+  exps = all_exps[[1]]
+  last_row = tail(exps,1)
+  
+  x_labs = labels
+  
+  ddf = t(as.data.frame(lapply(x_labs, function(l) extract_row(last_row,l))))
+  rownames(ddf) = x_labs
+  colnames(ddf) = c("label","mean","sep","sem")
+  
+  ddf = as.data.frame(ddf)
+  ddf$mean = as.numeric(as.character(ddf$mean))
+  ddf$sem = as.numeric(as.character(ddf$sem))
+  ddf$sep = as.numeric(as.character(ddf$sep))
+  #xrange = range(seq(1,length(x_labs)))
+  #yrange = range(min(as.numeric(ddf$mean)),max(as.numeric(ddf$mean)))
+  
+  ylabel = "finav"
+  ggplot(data=ddf,aes(x=label)) + 
+    geom_line(aes(y=mean,group=1),color="red") +
+    geom_line(aes(y=sep,group=1),color="green") +
+    geom_line(aes(y=sem,group=1),color="blue") +
+    ylab(ylabel) +
+    theme(axis.text.x=element_text(angle=40, hjust=1)) +
+    theme_xkcd() 
+}
+
+# --------
+
+
 #exps = list("qst/data/12688761706545524501_8585170111640645107/",
 #            "qst/data/12688761706545524501_17578986486117912121/",
 #            "qst/data/14292387654137485179_8585170111640645107/",
@@ -91,19 +128,37 @@ cluster_plots = function(all_exps,ylabel,labs,indices,cols) {
 #            "size=100 replace=true", 
 #            "size=100 replace=false")
 
+source("anal.R")
                                             
 #extracted_exps = across_reps(e,component,tag,reps,cycles)
 
-#unr_data = un_reported_data(exps,"two_cycle","score",0:9,0:300)
+#  VERY expensive
+unr_data = un_reported_data(exps,"two_cycle","score",0:19,0:1000)
+
+#  VERY expensive
+for_unr_data = un_reported_data(exps,"two_cycle","score",0:19,0:300)
 
 #all_max = compute_all(max,exps,labels,unr_data)
 
-#all_avg = compute_all(avg,exps,labels,unr_data)
+all_avg = compute_all(avg,exps,labels,for_unr_data)
+all_avg = compute_all(avg,exps,labels,unr_data)
 
-#p1 = cluster_plots(all_max,"max",labels, c(1,2),c("yellow", "blue")) 
+exps
+labels
+
+p1 = cluster_plots(all_avg,"avg",labels, 1:4, palette(rainbow(5)))
+p2 = cluster_plots(all_avg,"avg",labels, 5:7, palette(rainbow(4)))
+p3 = cluster_plots(all_avg,"avg",labels, 8:10, palette(rainbow(4)))
+
+p = cluster_plots(all_avg,"avg",labels, 1:10, palette(rainbow(11)))
+p
+
+f1 = final_fitness_plots(all_avg,"Final avg",labels)
+f1
 
 #p1 / p2
 
+x =palette(rainbow(5))
 #p2 = cluster_plots(all_avg,"avg",labels,c(1,2),c("red", "green")) 
   
 #p2
