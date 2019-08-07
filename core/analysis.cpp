aea88d6fbf2809acd45acf5a8b65596d5862bef3
cgnitash <cgnitash@gmail.com>
2019-08-07 10:40:12 -0400

    cleanup analysis

--- core/analysis.cpp
+++ core/analysis.cpp
@@ -19,26 +19,26 @@ namespace ded
 
 namespace experiments
 {
-void
-    analyse_all_simulations(std::string file_name, std::string trace_name)
+std::tuple<specs::Trace, std::string, std::vector<Simulation>>
+    get_single_trace(std::vector<Simulation> simulations,
+                     std::string             trace_name)
 {
 
-  std::vector<
-      std::pair<std::pair<specs::Trace, std::string>, std::vector<Simulation>>>
+  std::vector<std::tuple<specs::Trace, std::string, std::vector<Simulation>>>
       trace_paths;
 
-  auto simulations = ded::experiments::parse_all_simulations(file_name);
   for (auto sim : simulations)
   {
     for (auto trace : sim.traces)
     {
       auto key = ranges::find_if(trace_paths, [trace](auto t) {
-        return t.first.second == trace.second;
+        return std::get<1>(t) == trace.second;
       });
       if (key == ranges::end(trace_paths))
-        trace_paths.push_back({ trace, { sim } });
+        trace_paths.push_back(
+            { std::get<0>(trace), std::get<1>(trace), { sim } });
       else
-        key->second.push_back(sim);
+        std::get<2>(*key).push_back(sim);
     }
   }
 
@@ -51,11 +51,12 @@ void
   }
 
   auto base_trace_name = m[1].str();
+  auto trace_paths_back_up = trace_paths;
   trace_paths.erase(
       std::remove_if(std::begin(trace_paths),
                      std::end(trace_paths),
-                     [&base_trace_name](auto trace) {
-                       return trace.first.first.signal_.user_name() !=
+                     [&base_trace_name](auto t) {
+                       return std::get<0>(t).signal_.user_name() !=
                               base_trace_name;
                      }),
       std::end(trace_paths));
@@ -64,12 +65,22 @@ void
   {
     std::cout << "error: trace name " << base_trace_name
               << " not a valid trace\nvalid traces are\n";
-    for (auto t : trace_paths)
-      std::cout << t.first.second << "\n";
+    for (auto t : trace_paths_back_up)
+      std::cout << std::get<1>(t) << std::get<0>(t).signal_.user_name() << "\n";
     throw language::ParserError{};   // wrong exception
   }
 
-  unsigned int n = m[2].str().empty() ? 0 : std::stoi(m[2].str()) - 1;
+  if (trace_paths.size() > 1 && m[2].str().empty())
+  {
+    std::cout << "error: trace name " << base_trace_name
+              << " is repeated\nspecify trace as follows\n";
+    for (auto [i,t]  : ranges::view::enumerate(trace_paths))
+      std::cout << std::get<1>(t) << std::get<0>(t).signal_.user_name() << "~"
+                << i + 1 << "\n";
+    throw language::ParserError{};   // wrong exception
+  }
+
+  unsigned int n = m[2].str().empty() ? 0 : std::stoi(m[2].str().substr(1)) - 1;
 
   if (trace_paths.size() <= n)
   {
@@ -78,13 +89,14 @@ void
     throw language::ParserError{};   // wrong exception
   }
 
-  auto [trace_path, true_simulations] = trace_paths[n];
-  // auto &trace_path = trace_path_simulations.first;
-  // auto &true_simulations = trace_path_simulations.second;
+  return trace_paths[n];
+}
 
-  // auto trace      = *(simulations.begin()->traces.begin());
-  // auto true_trace_name = trace.second + trace.first.signal_.user_name() +
-  // "_";
+std::pair<std::vector<int>, int>
+    find_all_stored_data(
+        std::tuple<specs::Trace, std::string, std::vector<Simulation>> t)
+{
+  auto [trace, trace_path, true_simulations] = t;
   std::vector<int> rep_counts;
   std::vector<int> invocation_counts;
   for (const auto &sim : true_simulations)
@@ -92,21 +104,21 @@ void
     auto bar_code = sim.bar_code();
     for (auto rep : ranges::view::iota(0))
     {
-      auto rep_path = "data/" + bar_code + "/" + std::to_string(rep) + "/" +
-                      trace_path.second;
-      //std::cout << rep_path << "\n";
+      auto rep_path =
+          "data/" + bar_code + "/" + std::to_string(rep) + "/" + trace_path;
+      // std::cout << rep_path << "\n";
       if (!std::experimental::filesystem::exists(rep_path))
       {
         rep_counts.push_back(rep);
         break;
       }
-      int invocation = trace_path.first.frequency_;
+      int invocation = trace.frequency_;
       while (std::experimental::filesystem::exists(
-          rep_path + trace_path.first.signal_.user_name() + "_" +
+          rep_path + trace.signal_.user_name() + "_" +
           std::to_string(invocation) + ".csv"))
-        invocation += trace_path.first.frequency_;
-      invocation_counts.push_back(invocation - trace_path.first.frequency_);
-      //std::cout << invocation << "\n";
+        invocation += trace.frequency_;
+      invocation_counts.push_back(invocation - trace.frequency_);
+      // std::cout << invocation << "\n";
     }
   }
 
@@ -116,7 +128,6 @@ void
     std::cout << "warning: misalignment of replicates\nas many as " << *high_rep
               << " replicates exist\nminimum of " << *low_rep
               << " will be used\n";
-    return;
   }
   auto [low, high] = ranges::minmax_element(invocation_counts);
   if (*low != *high)
@@ -124,8 +135,22 @@ void
     std::cout << "warning: misalignment of trace invocation counts\nas many as "
               << *high << " invocations exist\nminimum of " << *low
               << " will be used\n";
-    return;
   }
+
+  return { ranges::view::iota(trace.frequency_, *low + 1) |
+               ranges::view::filter(
+                   [f = trace.frequency_](auto i) { return !(i % f); }),
+           *low_rep };
+}
+
+void
+    analyse_all_simulations(std::string file_name, std::string trace_name)
+{
+  auto simulations = ded::experiments::parse_all_simulations(file_name);
+  auto single      = get_single_trace(simulations, trace_name);
+
+  auto [trace, trace_path, true_simulations] = single;
+  auto [invokes, max_rep]                    = find_all_stored_data(single);
 
   std::ofstream analysis_file("analysis.R");
   analysis_file << "#!/usr/bin/env Rscript";
@@ -142,17 +167,16 @@ void
                     }) |
                     ranges::view::intersperse(",") | ranges::action::join)
                 << ")\n";
-  analysis_file << "data=un_reported_data(exps,\"/" + trace_path.second
-                << "\",\"" << trace_path.first.signal_.user_name()
-                << "\",0:" << *low_rep - 1 << ",seq("
-                << trace_path.first.frequency_ << "," << *low << ","
-                << trace_path.first.frequency_ << "))";
+  analysis_file << "data=un_reported_data(exps,\"/" + trace_path << "\",\""
+                << trace.signal_.user_name() << "\",0:" << max_rep - 1
+                << ",seq(" << trace.frequency_ << "," << invokes.back() << ","
+                << trace.frequency_ << "))";
   analysis_file << "\nall_avg = "
                    "compute_all(avg,exps,labels,data)\npdf(\"result.pdf\")\n";
   analysis_file << "cluster_plots(all_avg,\"avg\",labels,1:"
                 << simulations.size() << ",palette(rainbow("
                 << simulations.size() + 1 << ")),\"" << file_name
-                << " Replicates " << *low_rep << "\")\n";
+                << " Replicates " << max_rep << "\")\n";
   analysis_file << "final_fitness_plots(all_avg,\"Final avg\",labels)";
   analysis_file
       << "\nfor (i in 1:" << simulations.size()
--- core/analysis.h
+++ core/analysis.h
@@ -2,6 +2,7 @@
 #pragma once
 
 #include <string>
+#include "experiments.h"
 #include "configuration.h"
 namespace ded
 {
@@ -11,5 +12,9 @@ namespace experiments
 
 void analyse_all_simulations(std::string, std::string);
 
+std::tuple<specs::Trace, std::string, std::vector<Simulation>>
+    get_single_trace(std::vector<Simulation>, std::string);
+std::pair<std::vector<int>, int> find_all_stored_data(
+    std::tuple<specs::Trace, std::string, std::vector<Simulation>>);
 }
 }
