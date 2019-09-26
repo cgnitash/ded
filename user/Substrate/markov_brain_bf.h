30de2bcb99bf6834aa0a14e6db4103ca4acaee52
cgnitash <cgnitash@gmail.com>
2019-09-26 09:36:29 -0400

    started multiple tag matching

---
+++ user/Substrate/markov_brain_bf.h
@@ -0,0 +1,102 @@
+#pragma once
+
+#include "../../components.h"
+
+#include <algorithm>
+#include <vector>
+
+class markov_brain_bf
+{
+
+  std::string front_in_sense_ = "<double,inputs>";
+  std::string back_in_sense_  = "<double,inputs>";
+  std::string out_sense_      = "<double,outputs>";
+
+  ded::concepts::Encoding  genome_;
+  ded::specs::EncodingSpec genome_spec_ = genome_.publishConfiguration();
+  std::regex               encoding_parser_{ R"(([^:]+):)" };
+
+  long input_  = 10;
+  long output_ = 10;
+  long hidden_ = 10;
+
+  std::vector<double> buffer_;
+
+  std::vector<long> codon_{ 7, 14 };
+  long              gene_length_ = 28;
+
+  struct gate
+  {
+    std::vector<long> logic_, ins_, outs_;
+  };
+
+  std::vector<gate> gates_;
+
+  bool gates_are_computed_ = false;
+  void compute_gates_();
+  // void seed_gates_(size_t = 1);
+
+public:
+  markov_brain_bf()
+  {
+    configure(publishConfiguration());
+  }
+
+  ded::specs::SubstrateSpec
+      publishConfiguration()
+  {
+    ded::specs::SubstrateSpec es;
+
+    es.bindParameter("inputs", input_);
+    es.bindParameter("outputs", output_);
+    es.bindParameter("hiddens", hidden_);
+
+    es.bindInput("front_in_sense", "<double,inputs>");
+    es.bindInput("back_in_sense", "<double,inputs>");
+    es.bindOutput("out_sense", "<double,outputs>");
+
+    es.bindEncoding("genome", genome_spec_);
+
+    return es;
+  }
+
+  void
+      configure(ded::specs::SubstrateSpec es)
+  {
+
+    es.configureParameter("inputs", input_);
+    es.configureParameter("outputs", output_);
+    es.configureParameter("hiddens", hidden_);
+
+    es.configureInput("front_in_sense", front_in_sense_);
+    es.configureInput("back_in_sense", back_in_sense_);
+
+    es.configureOutput("out_sense", out_sense_);
+
+    es.configureEncoding("genome", genome_spec_);
+    genome_.configure(genome_spec_);
+
+    genome_.generate(500);
+    genome_.seedCodons(codon_, 4);
+
+    buffer_ = std::vector(2 * input_ + output_ + hidden_, 0.);
+  }
+
+  void                  reset();
+  void                  mutate();
+  void                  input(std::string, ded::concepts::Signal);
+  ded::concepts::Signal output(std::string);
+  void                  tick();
+  ded::concepts::Encoding
+      getEncoding() const
+  {
+    return genome_;
+  }
+  void
+      setEncoding(ded::concepts::Encoding e)
+  {
+    genome_ = e;
+    compute_gates_();
+  }
+  ded::concepts::Encoding parseEncoding(std::string);
+};
